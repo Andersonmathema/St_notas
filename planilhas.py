397f9af3f50651ac80b5491c5b4861505993c477
Andersonmathema <ander.math89@gmail.com>
2024-07-07 21:29:37 -0300

    ignore e inicio planilha

--- planilhas.py
+++ planilhas.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-
-
+data = 'C:\\Users\\anddr\\Downloads\\planilha_formatada.xlsx'
+planilha = pd.read_excel(data) 
+st.title("Tabela de notas")
+df = pd.DataFrame(planilha)
+st.dataframe(data=planilha, height=2000, use_container_width=True )
 
